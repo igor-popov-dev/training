ad58fbe0d33eb7cea6b2086be92ba310a9f38521
ipopov <igor.popov.develop@gmail.com>
2025-01-14 08:52:23 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		sourcemap: true, // Включите sourcemaps для диагностики
+		minify: 'esbuild' // Проверьте, какое минификационное средство используется
+	},
 	base: '/training/'
 });
